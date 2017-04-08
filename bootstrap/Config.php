dea59b6fb117f9d1dce3dd86b4ac61c0e98f48d3
ankush tripathi <ankushtripathi0996@yahoo.com>
2017-04-08 19:04:01 +0530

    fix bugs

--- bootstrap/Config.php
+++ bootstrap/Config.php
@@ -34,8 +34,8 @@ function config($mode){
 				'smtp_auth' => true,
 				'smtp_secure' => 'tls',
 				'host' => 'smtp.gmail.com',
-				'username' => 'ankushsmart11@gmail.com',
-				'password' => 'qchtcfribfsmwnmb',
+				'username' => '',
+				'password' => '',
 				'port' => 587,
 				'html' => true
 			],
